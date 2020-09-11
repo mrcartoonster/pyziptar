df4e6c385e1207883a96d4465c57893b1e251605
Mrnobody <mrcartoonster@icloud.com>
2020-09-11 16:36:56 +0000

    📦 NEW: PYZ-1 #time 30m #comment Working unziping command

--- pyziptar/main.py
+++ pyziptar/main.py
@@ -3,29 +3,42 @@
 from pathlib import Path
 from typing import Optional
 
-# from zipfile import ZipFile
+from zipfile import ZipFile
 import typer
 
-app = typer.Typer()
+app = typer.Typer(help="App that unzips zi files given.")
 
 
 @app.command()
 def zip(
-    path: Optional[Path] = typer.Option(
-        Path().cwd(),
-        "--path",
-        "-p",
+    file: Path = typer.Argument(
+        ...,
         exists=True,
         file_okay=True,
         dir_okay=False,
         resolve_path=True,
         help=(
             (
-                "If path is given, zip file extracted in that location."
-                "If no path's given, zip file will be extracted in current "
-                "working directoy."
+                "File will be extracted in current working directory. To "
+                "extract file in selected directory, pass --path/-p flag."
             )
         ),
     ),
+    names: Optional[bool] = typer.Option(
+        False,
+        "--names",
+        "-n",
+        help="List the file names within the zip file."
+    )
 ):
-    pass
+    if names:
+        with ZipFile(file, 'r') as n:
+            typer.echo(n.namelist())
+
+    else:
+        with ZipFile(file, 'r') as f:
+            f.extractall()
+
+
+if __name__ == "__main__":
+    app()
