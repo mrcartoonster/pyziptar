c0776617976d3b6c41c0b512aaf5afa5305f7f5e
Mrnobody <mrcartoonster@icloud.com>
2020-09-12 18:53:34 +0000

    📦 NEW: PYZ-1 #time 30m #comment setting up tests

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+
+@pytest.fixture(scope="module")
+def zipfile(tmpdir_factory):
+    pass
--- tests/test_pyziptar.py
+++ tests/test_pyziptar.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
+from typer.testing import CliRunner
+
 from pyziptar import __version__
+
+runner = CliRunner()
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+def test_app():
+    pass
